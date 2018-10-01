7af4f45886bf1318cebb0fa1b7eafaed9419a8d1
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-10-01 23:12:14 +0800

    add more class methods

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
+const asyncRetry = require('async-retry')
 const axios = require('axios')
 const Promise = require('bluebird')
 const pThrottle = require('p-throttle')
-const aRetry = require('async-retry')
-// const qs = require('qs')
 const requestPromise = require('request-promise')
 const sharp = require('sharp')
 const { parseString: parseXml } = require('xml2js')
 
 const defaults = {
+  failureRetries: 3,
   imageDimensionLimit: null,
   maxPerPage: 1000,
   rateLimit: 3,
+  jpegQuality: { quality: 70 },
+  pngCompressionLevel: { compressionLevel: 7 },
 }
 
 /** instance of Storyblok API interface */
@@ -43,25 +45,28 @@ class StoryblokApiClient {
   }
 
   /**
-   * Using 'sharp' library to generate data buffer
-   * image compression is applied accordingly
+   * get total number of existing assets
    *
-   * @param {string} filePath - absolute path to image file
-   * @param {boolean} compression - flag to compress image
-   * @param {number} dimLimit - resizing dimension limit value
-   * @returns {Promise<Buffer>} buffered image data
+   * @returns {number} get a count of existing assets
    */
-  bufferImage(
-    filePath,
-    compression = false,
-    dimLimit = defaults.imageDimensionLimit
-  ) {
-    const options = { failOnError: true }
-    const image = sharp(filePath, options).rotate()
-    return resizeImage(image, dimLimit)
-      .then(image => compressImage(image, compression))
-      .then(image => image.toBuffer())
+  countAssets() {
+    return this.getSpace()
+      .then(space => space.assets_count)
       .catch(error => Promise.reject(error))
+  }
+
+  /**
+   * get total number of existing stories
+   *
+   * Storyblok API's space info does not account 'folders' as stories, so "this.getSpace({ spaceId }).then(space => space.stories_count)" does not work for the purpose of this function
+   * must be manually retrieved
+   *
+   * @returns {number} get a count of existing stories
+   */
+  countStories() {
+    return this.get(`/${this.spaceId}/stories`)
+      .then(res => res.headers.total)
+      .catch(error => this.apiClient(error, 'countStories'))
   }
 
   /**
@@ -77,12 +82,39 @@ class StoryblokApiClient {
   }
 
   /**
+   * create a component on server
+   *
+   * @param {Object} definition - Storyblok component definition object
+   * @returns {Object} details of component that was created
+   */
+  createComponent(definition) {
+    const data = { component: definition }
+    return this.post(`/${this.spaceId}/components`, data)
+      .then(res => res.data.component)
+      .catch(error => apiErrorHandler(error, 'createComponent'))
+  }
+
+  /**
+   * create a content story on server
+   *
+   * @param {Object} storyData - Storyblok story data object
+   * @returns {Object} details of story that was created
+   */
+  createStory(storyData) {
+    const data = { story: storyData }
+    return this.post(`/${this.spaceId}/stories`, data)
+      .then(res => res.data.story)
+      .catch(error => apiErrorHandler(error, 'createStory'))
+  }
+
+  /**
    * register an image as a Storyblok asset and upload to server
+   * compression and resize using the `sharp.js` library
    *
    * @param {string} filePath - absolute file path to image
    * @param {boolean} compression - flag to compress image
    * @param {number} dimLimit - resize dimension limit value
-   * @returns {string} Public url to access the asset.
+   * @returns {string} public url to access the asset
    */
   createImageAsset(
     filePath,
@@ -91,7 +123,7 @@ class StoryblokApiClient {
   ) {
     const fileName = filePath.split('\\').pop()
     return Promise.all([
-      this.bufferImage(filePath, compression, dimLimit),
+      bufferImage(filePath, compression, dimLimit),
       this.signAsset(fileName),
     ])
       .then(([buffer, signedRequest]) => {
@@ -104,21 +136,53 @@ class StoryblokApiClient {
    * delete an asset from server by its id
    *
    * @param {number} assetId - id of the asset to be deleted
-   * @returns {number} assetId
+   * @returns {number} assetId is returned on success
    */
   deleteAsset(assetId) {
     return this.delete(`/${this.spaceId}/assets/${assetId}`)
       .then(() => assetId)
-      .catch(error => apiErrorHandler(error, 'deleteAsset'))
+      .catch(error => {
+        // allow continuation if the error is "not found (404)"
+        // sometimes an asset is registered but a corresponding physical file is not uploaded for some reason.  A delete request will result in 404 error, but the registered reference is removed successfully
+        if (parseInt(error.response.status) === 404) {
+          return Promise.resolve(assetId)
+        } else {
+          return apiErrorHandler(error, 'deleteAsset')
+        }
+      })
+  }
+
+  /**
+   * delete a specific asset folder
+   *
+   * @param {number} assetFolderId - id of asset folder to be deleted
+   * @returns {number} assetFolderId is returned on success
+   */
+  deleteAssetFolder(assetFolderId) {
+    return this.delete(`/${this.spaceId}/asset_folders/${assetFolderId}`)
+      .then(() => assetFolderId)
+      .catch(error => apiErrorHandler(error, 'deleteAssetFolder'))
+  }
+
+  /**
+   * delete a component from server by its id
+   *
+   * @param {number} componentId - id of component to be deleted
+   * @returns {number} componentId is returned on success
+   */
+  deleteComponent(componentId) {
+    return this.delete(`/${this.spaceId}/components/${componentId}`)
+      .then(() => componentId)
+      .catch(error => apiErrorHandler(error, 'deleteComponent'))
   }
 
   /**
    * delete all existing assets from server
    *
-   * @returns {number[]} array of asset id's that was removed
+   * @returns {number[]} array of asset id's that were removed
    */
   deleteExistingAssets() {
-    return this.getAssets()
+    return this.getExistingAssets()
       .then(existingAssets => {
         const mapFn = asset => this.deleteAsset(asset.id)
         return Promise.mapSeries(existingAssets, mapFn)
@@ -127,51 +191,144 @@ class StoryblokApiClient {
   }
 
   /**
-   * get total number of assets existing on server
+   * delete all existing components from server
    *
-   * @returns {number} count of existing assets
+   * @returns {number[]} array of component id's that were removed
    */
-  getAssetCount() {
-    return this.getSpace()
-      .then(space => space.assets_count)
+  deleteExistingComponents() {
+    return this.getExistingComponents()
+      .then(existingComponents => {
+        const mapFn = component => this.deleteComponent(component.id)
+        return Promise.mapSeries(existingComponents, mapFn)
+      })
+      .catch(error => Promise.reject(error))
+  }
+
+  /** delete all existing stories from server */
+  deleteExistingStories() {
+    const filterRootFoldersFn = story => {
+      let isAtRoot = story.parent_id === 0
+      let isFolder = story.is_folder === true
+      return isAtRoot && isFolder
+    }
+    const mapFn = story => this.deleteStory(story.id)
+    const deleteRootFolders = () => {
+      return this.getExistingStories()
+        .then(stories => stories.filter(filterRootFoldersFn))
+        .then(rootFolders => Promise.mapSeries(rootFolders, mapFn))
+        .catch(error => Promise.reject(error))
+    }
+    const deleteRootLevelStories = () => {
+      return this.getExistingStories()
+        .then(stories => Promise.mapSeries(stories, mapFn))
+        .catch(error => Promise.reject(error))
+    }
+    return deleteRootFolders()
+      .then(() => deleteRootLevelStories())
       .catch(error => Promise.reject(error))
   }
 
   /**
-   * get total number of pagination pages of existing assets on server
+   * delete a specific story from server
    *
-   * @param {number} perPage - number to list per page
-   * @returns {number} count of pagination pages
+   * @param {number} storyId - id of the story to be deleted
+   * @returns {number} id of story that was removed
    */
-  getAssetPaginationPageCount(perPage = defaults.maxPerPage) {
-    return this.getAssetCount()
-      .then(assetCount => Math.ceil(assetCount / perPage))
-      .catch(error => Promise.reject(error))
+  deleteStory(storyId) {
+    return this.delete(`/${this.spaceId}/stories/${storyId}`)
+      .then(() => storyId)
+      .catch(error => apiErrorHandler(error, 'deleteStory'))
+  }
+
+  /**
+   * get a component's definition from server by its id
+   *
+   * @param {number} componentId - id of component
+   * @returns {Object} component definition
+   */
+  getComponent(componentId) {
+    return this.get(`/${this.spaceId}/components/${componentId}`)
+      .then(res => res.data.component)
+      .catch(error => apiErrorHandler(error, 'getComponent'))
   }
 
   /**
    * get full listing of assets from server
    *
-   * @param {number} perPage - How many assets per page (defaults to server max limit of 1000).
-   * @returns {Object[]} Full list of existing assets
+   * @returns {Object[]} full list of existing assets
    */
-  getAssets(perPage = defaults.maxPerPage) {
-    return this.getSpace(space => Math.ceil(space.assets_count / perPage))
+  getExistingAssets() {
+    return this.countAssets()
+      .then(assetCount => Math.ceil(assetCount / defaults.maxPerPage))
       .then(pageCount => {
         const requests = Array.from(Array(pageCount).keys())
         const assets = []
         const mapFn = pageIndex => {
-          const per_page = perPage
-          const options = { params: { per_page, page: pageIndex + 1 } }
-          return this.get(`${this.spaceId}/assets`, options)
+          const per_page = defaults.maxPerPage
+          const page = pageIndex + 1
+          const params = { per_page, page }
+          return this.get(`${this.spaceId}/assets`, { params })
             .then(res => assets.push(...res.data.assets))
-            .catch(error => Promise.reject(error))
+            .catch(error => apiErrorHandler(error, 'getExistingAssets'))
         }
         return Promise.mapSeries(requests, mapFn)
           .then(() => assets)
           .catch(error => Promise.reject(error))
       })
-      .catch(error => apiErrorHandler(error, 'getAssets'))
+      .catch(error => Promise.reject(error))
+  }
+
+  /**
+   * get a list of existing asset folders from server
+   *
+   * @returns {Object[]} List of asset folder details
+   */
+  getExistingAssetFolders() {
+    const url = `/${this.spaceId}/asset_folders`
+    const query = `?search=`
+    return this.get(url + query)
+      .then(res => res.data.asset_folders)
+      .catch(error => apiErrorHandler(error, 'getExistingAssetFolders'))
+  }
+
+  /**
+   * get a list of existing component details from server
+   * (it is assumed that the working space has at most 1000 existing components)
+   *
+   * @returns {Object[]} List of component definitions
+   */
+  getExistingComponents() {
+    const options = { per_page: defaults.maxPerPage }
+    return this.get(`/${this.spaceId}/components`, options)
+      .then(res => res.data.components)
+      .catch(error => apiErrorHandler(error, 'getExistingComponents'))
+  }
+
+  /**
+   * get the complete list of content stories from server
+   *
+   * @returns {Object[]} full list of existing content stories
+   */
+  getExistingStories() {
+    // find out how many pagination pages existed
+    return this.countStories()
+      .then(storyCount => Math.ceil(storyCount / defaults.maxPerPage))
+      .then(pageCount => {
+        const requests = Array.from(Array(pageCount).keys())
+        const stories = []
+        const mapFn = pageIndex => {
+          const per_page = defaults.maxPerPage
+          const page = pageIndex + 1
+          const params = { per_page, page }
+          return this.get(`${this.spaceId}/stories`, { params })
+            .then(res => stories.push(...res.data.stories))
+            .catch(error => apiErrorHandler(error, 'getStories'))
+        }
+        return Promise.mapSeries(requests, mapFn)
+          .then(() => stories)
+          .catch(error => Promise.reject(error))
+      })
+      .catch(error => Promise.reject(error))
   }
 
   /**
@@ -186,7 +343,36 @@ class StoryblokApiClient {
   }
 
   /**
-   * Register a file as a Storyblok asset
+   * get details of a content story by its id
+   *
+   * @param {number} storyId - id of the content story
+   * @returns {Object} details of content story
+   */
+  getStory(storyId) {
+    return this.get(`/${this.spaceId}/stories/${storyId}`)
+      .then(res => res.data.story)
+      .catch(error => apiErrorHandler(error, 'getStory'))
+  }
+
+  /**
+   * move a story's sequential order
+   *
+   * @param {string} storyId - id of the story to be moved
+   * @param {string} afterId - to be positioned after story of this id
+   * @returns {Object} details of the moved story
+   */
+  reorderStory(storyId, afterId) {
+    const url = `/${this.spaceId}/stories/${storyId}/move`
+    const query = `?after_id=${afterId}`
+    return this.put(url + query)
+      .then(() => this.getStory(storyId))
+      .then(res => res.data.story)
+      .catch(error => apiErrorHandler(error, 'reorderStory'))
+  }
+
+  /**
+   * register a file as a Storyblok asset
+   * the physical file still has to be uploaded
    *
    * @param {string} filename - file name to be register
    * @returns {Object} asset signing info
@@ -198,10 +384,10 @@ class StoryblokApiClient {
   }
 
   /**
-   * Physically uploading an asset after it is registered with 'signAsset()' function.
+   * physically upload an asset after it is registered with 'signAsset()' method
    *
-   * @param {Buffer} buffer - Buffered asset.
-   * @param {Object} signedRequest - The returned object from 'signAsset()' function, containing info to enable the actual physical file upload
+   * @param {Buffer} buffer - buffered asset
+   * @param {Object} signedRequest - the server response from 'signAsset()' method
    * @returns {string} Url to access the asset
    */
   uploadAsset(buffer, signedRequest) {
@@ -213,13 +399,12 @@ class StoryblokApiClient {
       options: { filename, contentType },
     }
     const retryOptions = {
-      retries: 5,
+      retries: defaults.failureRetries,
       minTimeout: 100,
       maxTimout: 500,
-      randomize: true,
       onRetry: () => console.log('asset upload retried'),
     }
-    return aRetry((bail, attempt) => {
+    return asyncRetry((bail, attempt) => {
       return requestPromise({
         method: 'post',
         url: signedRequest.post_url,
@@ -227,19 +412,34 @@ class StoryblokApiClient {
       })
         .then(() => signedRequest.pretty_url)
         .catch(requestError => {
-          parseXml(requestError, { trim: true }, (parseError, result) => {
-            console.log(`${attempt}/${retryOptions.retries}`)
-            console.log(JSON.parse(result))
-            const isInternalError = requestError.status === 500
-            if (parseError || !isInternalError) {
-              bail(requestError || requestError)
-              return
-            }
-          })
+          if (requestError.status === 500) {
+            parseXml(
+              requestError,
+              { trim: true },
+              (parseError, parsedErrorMessage) => {
+                if (parseError) {
+                  bail(parseError)
+                  return
+                }
+                console.log(`${attempt - 1}/${retryOptions.retries}`)
+                console.log(JSON.parse(parsedErrorMessage))
+              }
+            )
+          }
         })
     }, retryOptions)
       .then(prettyUrl => prettyUrl)
-      .catch(error => Promise.reject(error))
+      .catch(requestError => {
+        parseXml(
+          requestError,
+          { trim: true },
+          (parseError, parsedErrorMessage) => {
+            if (parseError) return Promise.reject(parseError)
+            const error = new Error(JSON.parse(parsedErrorMessage))
+            return Promise.reject(error)
+          }
+        )
+      })
   }
 }
 
@@ -260,6 +460,28 @@ function apiErrorHandler(error, fnName) {
     console.log('Error:', error.message)
   }
   throw error
+}
+
+/**
+ * Using 'sharp' library to generate data buffer
+ * image compression is applied accordingly
+ *
+ * @param {string} filePath - absolute path to image file
+ * @param {boolean} compression - flag to compress image
+ * @param {number} dimLimit - resizing dimension limit value
+ * @returns {Promise<Buffer>} buffered image data
+ */
+function bufferImage(
+  filePath,
+  compression = false,
+  dimLimit = defaults.imageDimensionLimit
+) {
+  const options = { failOnError: true }
+  const image = sharp(filePath, options).rotate()
+  return resizeImage(image, dimLimit)
+    .then(image => compressImage(image, compression))
+    .then(image => image.toBuffer())
+    .catch(error => Promise.reject(error))
 }
 
 /**
