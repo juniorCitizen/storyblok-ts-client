ed54fdb13f5ab48e7918a5404f4167f594d03892
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-10-29 12:16:56 +0800

    recommit files that were not uploaded correctly

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { ApiClient, retrySettings } from './classes/ApiClient';
-export { Storyblok } from './classes/Storyblok';
-export * from './interfaces';
-//# sourceMappingURL=index.d.ts.map
--- dist/interfaces/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './assets';
-export * from './assetFolders';
-export * from './components';
-export * from './spaces';
-export * from './stories';
-//# sourceMappingURL=index.d.ts.map
