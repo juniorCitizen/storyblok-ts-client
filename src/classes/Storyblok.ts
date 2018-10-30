047d4032b428348ee4fb46a83cd32ec729cd6ea1
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-10-30 09:36:26 +0800

    add random variance to retryDelay in attempt to reduce retry clash

--- src/classes/Storyblok.ts
+++ src/classes/Storyblok.ts
@@ -269,7 +269,8 @@ export class Storyblok implements IStoryblokClass {
           } else {
             config.retryCount += 1
             return new Promise(resolve => {
-              const delay: number = config.retryDelay || 1000
+              const variance = Math.floor(Math.random() * 500) + 1
+              const delay: number = (config.retryDelay || 1250) - variance
               const factor: number = config.retryCount as number
               setTimeout(() => {
                 console.log('status:', response.status)
